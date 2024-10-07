cb3aec14a54fe0330527957517714374536ee8cc
Ajsen Martynov <ajsmartmin@gmail.com>
2024-10-07 14:00:28 +0900

    Create locales for i18next

---
+++ frontend/src/locales/ru/index.js
@@ -0,0 +1,33 @@
+const ru = {
+  translation: {
+    messagesHeader: {
+      messagesCount_zero: '{{count}} сообщений',
+      messagesCount_one: '{{count}} сообщение',
+      messagesCount_few: '{{count}} сообщения',
+      messagesCount_many: '{{count}} сообщений',
+      messagesCount_other: '{{count}} сообщений',
+    },
+    loginPage: {
+      footer: {
+        registration: 'Регистрация',
+        doNotHaveAnAccount: 'Нет аккаунта?',
+      },
+    },
+    modal: {
+      removeChannelDialog: {
+        title: 'Удалить канал',
+        removeButton: 'Удалить',
+        removeQuestion: 'Удалить?',
+      },
+      editChannelForm: {
+        title: 'Переименовать канал',
+      },
+      addChannelForm: {
+        title: 'Добавить канал',
+      },
+      cancelButton: 'Отменить',
+    },
+  },
+};
+
+export default ru;
