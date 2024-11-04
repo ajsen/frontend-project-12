2a137e042ba0cfe546bd9199abb023db6c943ba0
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-05 00:34:55 +0900

    create frontend/src/components/auth/components/LoginCardFooter.jsx

---
+++ frontend/src/components/auth/components/LoginCardFooter.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { CardFooter } from 'react-bootstrap';
+
+import ROUTE_PATHS from '../../../routes/routePaths';
+
+const LoginCardFooter = () => {
+  const { t } = useTranslation();
+
+  return (
+    <CardFooter className="p-4">
+      <div className="text-center">
+        <span className="me-1">
+          {`${t('noAccount')}?`}
+        </span>
+        <Link to={ROUTE_PATHS.signupPage}>
+          {t('registration')}
+        </Link>
+      </div>
+    </CardFooter>
+  );
+};
+
+export default LoginCardFooter;
