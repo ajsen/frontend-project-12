475ffa8ecf8c0878ba899d912355a62a49043dd0
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-04 23:48:54 +0900

    create frontend/src/slices/index.js

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,21 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-// import apiSlice from '../api/apiSlice';
+import apiSlice from '../api/apiSlice';
 import authReducer from './authSlice';
 import channelsReducer from './channelsSlice';
-import messagesReducer from './messagesSlice';
+import userUiReducer from './userUiSlice';
 import { listenerMiddleware } from './listenerMiddleware';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     channels: channelsReducer,
-    messages: messagesReducer,
-    // [apiSlice.reducerPath]: apiSlice.reducer,
+    userUi: userUiReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-    .prepend(listenerMiddleware.middleware)
-  // .concat(apiSlice.middleware),
+    .concat(apiSlice.middleware)
+    .prepend(listenerMiddleware.middleware),
 });
 
 export default store;
