165cc055b4a35fa0d16ba627c5a8eaa37650bf7f
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-04 23:57:53 +0900

    update frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client';
 
 import './assets/index.css';
 import './assets/application.scss';
+import 'react-toastify/dist/ReactToastify.css';
 
-import i18n from './utils/i18n';
-import App from './App';
+import init from './init';
 
 const mountNode = document.getElementById('root');
 const root = createRoot(mountNode);
 
 root.render(
   <React.StrictMode>
-    <App />
+    {await init()}
   </React.StrictMode>
 );
