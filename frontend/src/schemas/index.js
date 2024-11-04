d58c7f5af71611a78a5fd798506d3cb030ad7f86
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-05 00:29:36 +0900

    create frontend/src/schemas/index.js

---
+++ frontend/src/schemas/index.js
@@ -0,0 +1,34 @@
+import { object, string } from 'yup';
+
+export const loginFormValidationSchema = object({
+  username: string().trim().required(),
+  password: string().trim().required(),
+});
+
+export const signupFormValidationSchema = object({
+  username: string()
+    .trim()
+    .required()
+    .min(3, 'common.usernameRequiredLength')
+    .max(20, 'common.usernameRequiredLength'),
+  password: string()
+    .trim()
+    .required()
+    .min(6, 'common.passwordRequiredLength')
+    .max(20, 'common.passwordRequiredLength'),
+  confirmPassword: string()
+    .trim()
+    .required()
+    .test({
+      name: 'confirmPassword',
+      message: 'common.passwordsMustBeEqual',
+      test: (value, context) => value === context.parent.password,
+    }),
+});
+
+export const modalFormValidationSchema = (channelNames) => object({
+  name: string()
+    .trim()
+    .required()
+    .notOneOf(channelNames),
+});
