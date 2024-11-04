2a0ba452bdda647706ff57b7bd2abecfc6ec397f
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-04 23:50:08 +0900

    create frontend/src/slices/userUiSlice.js

---
+++ frontend/src/slices/userUiSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const defaultLng = 'ru';
+
+const initialState = {
+  currentLng: defaultLng,
+  modalIsShown: false,
+};
+
+const userUiSlice = createSlice({
+  name: 'userUi',
+  initialState,
+  reducers: {
+    setModalIsShown: (state, action) => {
+      state.modalIsShown = action.payload;
+    },
+  },
+});
+
+export default userUiSlice.reducer;
+
+export const { setModalIsShown } = userUiSlice.actions;
+
+export const selectCurrentLng = (state) => state.userUi.currentLng;
+export const selectModalIsShown = (state) => state.userUi.modalIsShown;
