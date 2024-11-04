43e5357995056ae3603dcbe435b5407a97d9d137
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-04 23:56:43 +0900

    remove interceptors, getAuthHeader

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,31 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  timeout: 2000,
+  timeout: 10000,
 });
-
-const getAuthHeader = () => {
-  const credentials = JSON.parse(localStorage.getItem('credentials'));
-  return credentials?.token ? { Authorization: `Bearer ${credentials.token}` } : {}
-};
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const authHeader = getAuthHeader();
-    if (authHeader.Authorization) {
-      config.headers = {
-        ...config.headers,
-        ...authHeader,
-      };
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => Promise.reject(error),
-);
 
 export default axiosInstance;
