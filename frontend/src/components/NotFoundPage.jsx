e934b36d2a34d31f89faa427eb7e2990a32e027e
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-05 00:35:58 +0900

    create frontend/src/components/NotFoundPage.jsx

---
+++ frontend/src/components/NotFoundPage.jsx
@@ -0,0 +1,32 @@
+import { Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import ROUTE_PATHS from '../routes/routePaths';
+import image from '../assets/404.svg';
+
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="text-center">
+      <Image
+        src={image}
+        alt={t('pageNotFound')}
+        className="h-25"
+        fluid
+      />
+      <h1 className="h4 text-muted">
+        t('pageNotFound')
+      </h1>
+      <p className="text-muted">
+        {t('butYouCanFollow')}
+        <Link to={ROUTE_PATHS.mainPage} className="ms-1">
+          {t('toMainPage')}
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
