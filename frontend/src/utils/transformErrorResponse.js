f44e9ef7f07308ce0da46b3c17e773f881ebdb48
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-11 19:54:25 +0900

    Refactor frontend/src/utils/transformErrorResponse.js

--- frontend/src/utils/transformErrorResponse.js
+++ frontend/src/utils/transformErrorResponse.js
@@ -1,16 +1,8 @@
-const networkErrorStatuses = {
-  401: 'feedbackMessages.incorrectUsernameOrPassword',
-  409: 'feedbackMessages.usernameTaken',
-  500: 'feedbackMessages.internalServerError',
-};
+import handleNetworkError from './handleNetworkError';
 
-const networkErrorCodes = {
-  ERR_NETWORK: 'feedbackMessages.networkError',
-  ECONNABORTED: 'feedbackMessages.connectionTimeout',
-};
-
-const transformErrorResponse = ({ status, code }) => (status)
-  ? networkErrorStatuses[status] ?? 'feedbackMessages.unknownError'
-  : networkErrorCodes[code] ?? 'feedbackMessages.unknownError';
+const transformErrorResponse = (error) => handleNetworkError({
+  status: error.status ?? null,
+  code: error.code ?? null,
+});
 
 export default transformErrorResponse;
