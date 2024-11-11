ab8752605efbc0467ba7f4dc27e05e2212bce4f0
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-11 19:54:03 +0900

    Create frontend/src/utils/handleNetworkError.js

---
+++ frontend/src/utils/handleNetworkError.js
@@ -0,0 +1,18 @@
+const unknownError = 'feedbackMessages.unknownError';
+
+const messagesByStatus = {
+  401: 'feedbackMessages.incorrectUsernameOrPassword',
+  409: 'feedbackMessages.usernameTaken',
+  500: 'feedbackMessages.internalServerError',
+};
+
+const messagesByCode = {
+  ERR_NETWORK: 'feedbackMessages.networkError',
+  ECONNABORTED: 'feedbackMessages.connectionTimeout',
+};
+
+const handleNetworkError = ({ status, code }) => (status)
+  ? messagesByStatus[status] ?? unknownError
+  : messagesByCode[code] ?? unknownError;
+
+export default handleNetworkError;
