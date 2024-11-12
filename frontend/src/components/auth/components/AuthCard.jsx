e249fb6b9aa6d4e113ac09d597136c495847fd2f
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-13 02:59:47 +0900

    Add line breaks for bootstrap components import

--- frontend/src/components/auth/components/AuthCard.jsx
+++ frontend/src/components/auth/components/AuthCard.jsx
@@ -1,10 +1,15 @@
-import { Card, Col, Image, Row } from 'react-bootstrap';
+import {
+  Card,
+  Col,
+  Image,
+  Row,
+} from 'react-bootstrap';
 
 const AuthCard = ({ form, footer = null, ...restProps }) => (
   <Card className="shadow-sm">
     <Row as={Card.Body} className="p-5">
       <Col xs={12} md={6} className="d-flex mb-3 mb-md-0 align-items-center justify-content-center">
-{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Image {...restProps} />
       </Col>
       {form}
