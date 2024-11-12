53e323364c3c2cac5e44a5183010e9dcb5bb730f
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-13 03:16:42 +0900

    Wrap the hash in a span tag

--- frontend/src/components/Messages/MessagesHeader.jsx
+++ frontend/src/components/Messages/MessagesHeader.jsx
@@ -13,7 +13,8 @@ const MessagesHeader = () => {
     <div className="bg-light mb-4 p-3 shadow-sm-small">
       <p className="m-0">
         <b>
-          # {currentChannel?.name}
+          <span className="me-1">#</span>
+          {currentChannel?.name}
         </b>
       </p>
       <span className="text-muted">
