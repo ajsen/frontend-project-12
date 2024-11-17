cf460237b46b164dc21c990364a1a0bb81510d4a
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-18 03:30:17 +0900

    Add SocketContext

--- frontend/src/contexts/index.js
+++ frontend/src/contexts/index.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
 export const ModalContext = createContext();
-
+export const SocketContext = createContext();
 export const ProfanityFilterContext = createContext();
