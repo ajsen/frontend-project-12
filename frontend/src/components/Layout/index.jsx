e0ee943f9177f627f32c35013c024994db5ce405
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-19 18:08:48 +0900

    Move ToastContainer, Modal to App

--- frontend/src/components/Layout/index.jsx
+++ frontend/src/components/Layout/index.jsx
@@ -1,24 +1,17 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 
 import Navbar from './Navbar';
-import Modal from '../Modal';
 import LoadingSpinner from '../common/LoadingSpinner';
 
 const Layout = () => (
-  <>
-    <Modal />
-    <div className="d-flex flex-column h-100">
-      <Navbar />
+  <div className="d-flex flex-column h-100">
+    <Navbar />
 
-      <Suspense fallback={<LoadingSpinner />}>
-        <Outlet />
-      </Suspense>
-
-    </div>
-    <ToastContainer />
-  </>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Outlet />
+    </Suspense>
+  </div>
 );
 
 export default Layout;
