d412fcec8fdda985cdb4f2ed9aa3d1acab732c63
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-26 02:06:49 +0900

    Refactor: get token from the state

--- frontend/src/api/apiSlice.js
+++ frontend/src/api/apiSlice.js
@@ -3,17 +3,15 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 import axiosBaseQuery from './axiosBaseQuery';
 import { api } from './apiPaths';
 
-const getAuthHeader = () => {
-  const userAuthData = JSON.parse(localStorage.getItem('userAuthData'));
-  return userAuthData?.token ? { Authorization: `Bearer ${userAuthData.token}` } : {};
-};
+const getAuthHeader = (token) => (token ? { Authorization: `Bearer ${token}` } : {});
 
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: axiosBaseQuery({
     baseUrl: api,
-    prepareHeaders: (headers) => {
-      const authHeader = getAuthHeader();
+    prepareHeaders: (headers, { getState }) => {
+      const { auth: { token } } = getState();
+      const authHeader = getAuthHeader(token);
       return (authHeader.Authorization)
         ? { ...headers, ...authHeader }
         : headers;
