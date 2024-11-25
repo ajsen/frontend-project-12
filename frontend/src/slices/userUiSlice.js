71b23cd8d353e03f25a4c0f855e6280e73bc4757
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-26 02:52:46 +0900

    Add showModal, hideModal reducers

--- frontend/src/slices/userUiSlice.js
+++ frontend/src/slices/userUiSlice.js
@@ -5,19 +5,28 @@ const defaultLng = 'ru';
 const initialState = {
   currentLng: defaultLng,
   modalIsShown: false,
+  modalType: null,
 };
 
 const userUiSlice = createSlice({
   name: 'userUi',
   initialState,
   reducers: {
-    setModalIsShown: (state, action) => {
+    showModal: (state, { payload: { isShown, modalType } }) => {
       // eslint-disable-next-line no-param-reassign
-      state.modalIsShown = action.payload;
+      state.modalIsShown = isShown;
+      // eslint-disable-next-line no-param-reassign
+      state.modalType = modalType;
+    },
+    hideModal: (state) => {
+      // eslint-disable-next-line no-param-reassign
+      state.modalIsShown = false;
+      // eslint-disable-next-line no-param-reassign
+      state.modalType = null;
     },
   },
 });
 
 export default userUiSlice.reducer;
 
-export const { setModalIsShown } = userUiSlice.actions;
+export const { showModal, hideModal } = userUiSlice.actions;
