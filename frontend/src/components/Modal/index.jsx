acd0369e0e20dc8f20b82773af1a01d463253776
Ajsen Martynov <ajsmartmin@gmail.com>
2024-11-26 02:47:15 +0900

    Move modal handler to component

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -1,32 +1,34 @@
 import { Modal as BsModal } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
 
-import useModal from '../../hooks/useModal';
-import { selectModalIsShown } from '../../slices/selectors';
+import useModalContent from '../../hooks/useModalContent';
+import { hideModal } from '../../slices/userUiSlice';
+import { selectModalIsShown, selectModalType } from '../../slices/selectors';
 
 const Modal = () => {
-  const { t } = useTranslation();
+  const dispatch = useDispatch();
+
   const modalIsShown = useSelector(selectModalIsShown);
-  const {
-    hideModal,
-    modalTitle,
-    modalContent,
-  } = useModal();
+  const modalType = useSelector(selectModalType);
+  const { title, body } = useModalContent(modalType);
+
+  const handleHideModal = () => {
+    dispatch(hideModal());
+  };
 
   return (
     <BsModal
       show={modalIsShown}
       centered
-      onHide={hideModal}
+      onHide={handleHideModal}
     >
-      <BsModal.Header onHide={hideModal} closeButton>
+      <BsModal.Header onHide={handleHideModal} closeButton>
         <BsModal.Title>
-          {t(modalTitle)}
+          {title}
         </BsModal.Title>
       </BsModal.Header>
       <BsModal.Body>
-        {modalContent}
+        {body}
       </BsModal.Body>
     </BsModal>
   );
